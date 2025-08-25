afa410bbd2cdabbd39676c20e3220c3d48ae27aa
Carmelo Chery <cchery@solucionesinformaticasmj.com>
2025-08-25 15:00:58 +0200

    COUNTRY 2 COMMIT

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { ListComponent } from "../../components/list/list.component";
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -9,7 +10,21 @@ import { ListComponent } from "../../components/list/list.component";
   styles: ``
 })
 export class ByCapitalPageComponent {
+  public countryService = inject(CountryService);
+
   onSearch(value: string){
-    console.log(value);
+    this.countryService.searchByCapital(value)
+    .subscribe({
+      next: (resp) => {
+        console.log(resp);
+      },
+      error: (error) => {
+        if(error.status == 404){
+          console.log(error.message);
+        }else{
+          console.log(error.status);
+        }
+      }
+    });
   }
 }
